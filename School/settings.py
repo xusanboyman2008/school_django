c80b2b3f6a2b920ad50f5c703282cc78c808ce54
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-01-26 10:20:32 +0500

    asd

--- School/settings.py
+++ School/settings.py
@@ -124,8 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_ROOT = BASE_DIR / 'static/images'
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
