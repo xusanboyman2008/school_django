ce812abd56b3f8dcdc7b0e97df72ffd3fb2a3397
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-01-26 10:52:43 +0500

    as

--- School/settings.py
+++ School/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'School.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
