59ab448dd6dae8ba87f79d014b87c59f0bc6a62f
xusanboyman2008 <abdulkhaevtolibjon55@gmail.com>
2025-02-01 20:59:00 +0500

    asasdasda

--- Logins/models.py
+++ Logins/models.py
@@ -17,6 +17,7 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     tg_id = models.BigIntegerField()
+    role = models.CharField(max_length=100,default="user")
     sending = models.BooleanField(default=False)
     def __str__(self):
         return self.name
